2cff690192986106d4b1a031e87f2c40ab3ab607
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-09-12 15:37:01 +0100

    New def for BasePage class

--- tests/base_page.py
+++ tests/base_page.py
@@ -1,11 +1,9 @@
 """
 POM base page
 """
-from loguru import logger
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from utils.config import BASE_URL, ACCOUNT_URL
+from utils.config import BASE_URL
 
 class BasePage:
     """
@@ -22,24 +20,31 @@ class BasePage:
         return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
                                                       message = f"Can't find element by locator {locator}")
     
+    def find_clickable_element(self, locator, time = 10):
+        """
+        Find clickable and visible element with waiting
+        """
+        element = WebDriverWait(self.driver, time).until(EC.visibility_of_element_located(locator),
+                                                      message = f"Can't find visible element by locator {locator}")
+        return WebDriverWait(self.driver, time).until(EC.element_to_be_clickable(element),
+                                                      message = f"Can't find clickable element by locator {locator}")
+    
     def find_elements(self, locator, time = 10):
         """
         Find elements with waiting
         """
         return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
                                                       message = f"Can't find elements by locator {locator}")
-    
+
+    def check_url(self, locator, time = 10):
+        """
+        Check URL with waiting
+        """
+        return WebDriverWait(self.driver, time).until(EC.url_contains(locator),
+                                                      message = f"URL doesn't contain {locator}")
+
     def go_to_site(self):
         """
         Get base URL
         """
         return self.driver.get(self.base_url)
-
-    def go_to_sign_in(self):
-        """
-        Go to Sign in
-        """
-        logger.info('Find element')
-        self.find_element(By.CLASS_NAME, 'data-testid="header-sign-in-button"').click()
-        logger.info('Wait for page')
-        WebDriverWait(self.driver, timeout = 5, poll_frequency = 1).until(EC.url_to_be(f"{ACCOUNT_URL}/sign-in"))
