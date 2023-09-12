0a3d64374e39bd3ba32ec30e4299d2cb963e6e58
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-09-12 15:40:30 +0100

    Classes for main and login pages

---
+++ pages/base_page.py
@@ -0,0 +1,50 @@
+"""
+POM base page
+"""
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils.config import BASE_URL
+
+class BasePage:
+    """
+    Base page
+    """
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_url = BASE_URL
+
+    def find_element(self, locator, time = 10):
+        """
+        Find element with waiting
+        """
+        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+                                                      message = f"Can't find element by locator {locator}")
+    
+    def find_clickable_element(self, locator, time = 10):
+        """
+        Find clickable and visible element with waiting
+        """
+        element = WebDriverWait(self.driver, time).until(EC.visibility_of_element_located(locator),
+                                                      message = f"Can't find visible element by locator {locator}")
+        return WebDriverWait(self.driver, time).until(EC.element_to_be_clickable(element),
+                                                      message = f"Can't find clickable element by locator {locator}")
+    
+    def find_elements(self, locator, time = 10):
+        """
+        Find elements with waiting
+        """
+        return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
+                                                      message = f"Can't find elements by locator {locator}")
+
+    def check_url(self, locator, time = 10):
+        """
+        Check URL with waiting
+        """
+        return WebDriverWait(self.driver, time).until(EC.url_contains(locator),
+                                                      message = f"URL doesn't contain {locator}")
+
+    def go_to_site(self):
+        """
+        Get base URL
+        """
+        return self.driver.get(self.base_url)
---
+++ pages/login_page.py
@@ -0,0 +1,50 @@
+"""
+Login Page
+"""
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from tests.base_page import BasePage
+
+class LoginLocators:
+    """
+    Locators for login page
+    """
+    LOGIN = (By.CSS_SELECTOR, '[data-testid*="header-sign-in-button"]')
+    BUTTON = (By.CSS_SELECTOR, '[type*="submit"]')
+
+class Login(BasePage):
+    """
+    Class for login
+    """
+    def __init__(self, browser):
+        super().__init__(browser)
+        self.browser = browser
+
+    def login_click(self):
+        """
+        Login button click
+        """
+        login_button = self.find_element(LoginLocators.LOGIN)
+        login_button.click()
+    
+    def submit_button_click(self):
+        """
+        Submit button click
+        """
+        submit_button = self.find_element(LoginLocators.BUTTON)
+        submit_button.click()
+
+    def click_and_hold(self, locator):
+        """
+        Button click and hold
+        """
+        button = self.find_clickable_element(locator)
+
+        actions = ActionChains(self.browser)
+        actions.click_and_hold(button).perform()
+        time.sleep(8)
+        actions.release().perform()
+        time.sleep(5)
+        
