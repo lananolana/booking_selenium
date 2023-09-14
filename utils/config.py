921492327e1d74f1e049a505a84d421e83ca4e45
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-09-14 22:32:34 +0100

    Search config

--- utils/config.py
+++ utils/config.py
@@ -3,5 +3,13 @@ BASE_URL = "https://www.booking.com"
 ACCOUNT_URL = "https://account.booking.com/"
 
 # Login data
-USERNAME = 'YOUR EMAIL'
+USERNAME = 'YOUR USERNAME'
 PASSWORD = 'YOUR PASSWORD'
+
+# Invalid login data
+USERNAME_WITHOUT_AT_SIGN = 'YOUR USERNAME WITHOUT @'
+
+# Search config
+CITY = "Porto"
+CHECK_IN = "2023-09-17"
+CHECK_OUT = "2023-09-25"
