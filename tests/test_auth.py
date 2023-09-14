69ce3eedaf4ebeefd25b26e3f3decca80d77f01b
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-09-14 22:24:33 +0100

    Import change

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,25 +1,8 @@
 """
 Authorization flow
 """
-import pytest
-
-from loguru import logger
 from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
-from pages.login_page import Login
-from utils.helpers import InputHelper
 from utils.config import BASE_URL, ACCOUNT_URL, USERNAME, USERNAME_WITHOUT_AT_SIGN, PASSWORD
-
-@pytest.fixture(scope = "module")
-def common_classes(browser):
-    """
-    Classes for tests
-    """
-    login_page = Login(browser)
-    base_page = BasePage(browser)
-    input_helper = InputHelper(browser)
-
-    return login_page, base_page, input_helper
 
 def test_robot_auth(browser, common_classes):
     """
@@ -32,11 +15,11 @@ def test_robot_auth(browser, common_classes):
     base_page.find_clickable_element((By.ID, "username"))
 
     input_helper.enter_input(input_id = 'username', data = f"{USERNAME}")
-    login_page.submit_button_click()
+    base_page.submit_button_click()
 
     base_page.check_url((f"{ACCOUNT_URL}sign-in/password"))
     input_helper.enter_input(input_id = 'password', data = f"{PASSWORD}")
-    login_page.submit_button_click()
+    base_page.submit_button_click()
 
     login_page.click_and_hold((By.ID, "px-captcha"))
 
@@ -53,7 +36,7 @@ def test_no_email(browser, common_classes):
 
     login_page.login_click()
     base_page.find_clickable_element((By.ID, "username"))
-    login_page.submit_button_click()
+    base_page.submit_button_click()
 
     alert = base_page.find_element((By.ID, "username-note"))
     assert alert.text == "Enter your email address", "Unexpected username note"
@@ -69,7 +52,7 @@ def test_new_email(browser, common_classes):
     login_page.login_click()
     base_page.find_clickable_element((By.ID, "username"))
     input_helper.enter_input(input_id = 'username', data = f"new{USERNAME}")
-    login_page.submit_button_click()
+    base_page.submit_button_click()
 
     base_page.check_url((f"{ACCOUNT_URL}register/password")) 
     page_header = base_page.find_element((By.CSS_SELECTOR, '[class="page-header"] h1'), 15)
@@ -89,7 +72,7 @@ def test_invalid_email(browser, common_classes):
     login_page.login_click()
     base_page.find_clickable_element((By.ID, "username"))
     input_helper.enter_input(input_id = 'username', data = f"{USERNAME_WITHOUT_AT_SIGN}")
-    login_page.submit_button_click()
+    base_page.submit_button_click()
 
     alert = base_page.find_element((By.ID, "username-note"))
     assert alert.text == "Make sure the email address you entered is correct.", "Unexpected username note"
